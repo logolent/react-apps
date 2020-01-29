9be39f61ff8c01a6d7bb8ab559335f8c8af1d80a
Logolent <email>
2020-01-29 13:35:07 +0400

    addded reselect

--- src/selectors/todoSelectors.js
+++ src/selectors/todoSelectors.js
@@ -1,4 +1,14 @@
+import { createSelector } from 'reselect';
+
 export const getCurrentTodos = ({todo}) => todo.todos;
 export const isTodosExist = state => Boolean(getCurrentTodos(state).length);
 export const isLoading = ({todo}) => todo.loading;
-export const getActiveTodosCount = ({todo}) => todo.todos.filter(todo => !todo.completed).length;
+export const getActiveTodosCount = createSelector(
+    getCurrentTodos,
+    todos => todos.filter(todo => !todo.completed).length
+);
+
+
+
+
+
